335f5f83774ff8e8bd0d4dcf00bdad75aea5fa2c
Jason Babie <automatard@gmail.com>
2023-02-16 11:39:40 -0800

    Another attempt

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (failure)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = sizeof(str) - 1;
+	ssize_t written = 0;
+	written = write(2, str, len);
+	if (written != len)
+		return (1);
+	return (1);
+}
