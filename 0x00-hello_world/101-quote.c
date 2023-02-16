d80eb3df31aa2b808dcf3f1823bda2518ff418c0
Jason Babie <automatard@gmail.com>
2023-02-16 11:43:07 -0800

    Another attempt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,9 @@ int main(void)
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = sizeof(str) - 1;
 	ssize_t written = 0;
+
 	written = write(2, str, len);
+
 	if (written != len)
 		return (1);
 	return (1);
